245ab1e8b40cd07b9f989f0aed35df26139cec7e
riefkyarif <riefkyar.ed@protonmail.com>
2022-02-18 23:44:27 +0700

    add layer cnn

--- CNN-Augment.py
+++ CNN-Augment.py
@@ -128,3 +128,22 @@ plt.show()
 datagen.fit(x_train)
 ####################################################################################################
 
+model = tf.keras.models.Sequential([
+                                    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(224, 224, 3)),
+                                    tf.keras.layers.Dropout(.2),
+                                    tf.keras.layers.MaxPooling2D(2, 2),
+                                    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+                                    tf.keras.layers.MaxPooling2D(2,2),
+                                    #tf.keras.layers.Dropout(.2),
+                                    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+                                    tf.keras.layers.MaxPooling2D(2,2),
+                                    #tf.keras.layers.Dropout(.5),
+                                    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+                                    tf.keras.layers.MaxPooling2D(2,2),
+                                    tf.keras.layers.Dropout(.2),
+                                    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+                                    tf.keras.layers.MaxPooling2D(2,2),
+                                    tf.keras.layers.Dropout(.1),
+                                    tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(512, activation='relu'),
+                                    tf.keras.layers.Dense(1, activation='sigmoid')])
