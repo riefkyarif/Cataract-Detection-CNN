9362cf004413b4b14a5c7843b27ade8910e229d4
riefkyarif <riefkyar.ed@protonmail.com>
2022-02-20 21:44:30 +0700

    graph

--- CNN-Augment.py
+++ CNN-Augment.py
@@ -172,3 +172,8 @@ def plot_accuracy_and_loss(train_model):
     val_acc = hist['val_accuracy']
     loss = hist['loss']
     val_loss = hist['val_loss']
+    epochs = range(len(acc))
+    f, ax = plt.subplots(1,2, figsize=(14,6))
+    ax[0].plot(epochs, acc, 'g', label='Training accuracy')
+    ax[0].plot(epochs, val_acc, 'r', label='Validation accuracy')
+    ax[0].set_title('Training and validation accuracy')
